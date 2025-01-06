206088923df29dd03f72594edf2bc200360555ef
Hassan Muhammad <hassan.brombaly@gmail.com>
2025-01-06 19:02:01 +0200

    update port

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Codeforces Points Filtration
 
-An app that enteres N number of contests and points for each question and returns a list of the handles 
+An app that enteres N number of contests and points for each question and returns a list of the handles
 and points
+
+### To run:
+
+1. Clone the repo.
+
+2. Install the required packages: `npm i`
+
+3. Create a .env file and insert the following:\
+		KEY \
+		SECRET
+
+4. Run the app using `npm run start:dev`, it should be up on port 4000
--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,12 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
     });
-    
+
 
 export const codeForcesClient = new CodeforcesClient(process.env.KEY, process.env.SECRET);
 app.use("/filter",filterRouter)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 }
